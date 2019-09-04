82f95e864a0ab4d844c3b4599d44055505f593f7
justin <jvanderheide@roc-dev.com>
2019-09-04 22:06:20 +0200

    Ho2

--- src/H02/Demo.java
+++ src/H02/Demo.java
@@ -8,7 +8,11 @@ public class Demo extends Applet {
 
     @Override
     public void paint(Graphics g) {
+        setBackground(Color.yellow);
         g.setColor(Color.blue);
-        g.drawString("Hallo Justin!", 40, 40);
+        g.drawString("Justin", 40, 40);
+        g.setColor(Color.red);
+        g.drawString("van der Heide",40,50);
+
     }
 }
