54ee6d4ad0f65fbd7dd870dd6a76a2fb597fc3c9
justin <jvanderheide@roc-dev.com>
2019-09-10 10:48:55 +0200

    Ho2

---
+++ src/H05/Negatievengetallen.java
@@ -0,0 +1,25 @@
+package H05;
+
+import java.applet.*;
+import java.awt.*;
+
+public class Negatievengetallen extends Applet {
+
+        int a;
+        int b;
+        int uitkomst;
+
+        @Override
+        public void init() {
+            a = 2147483647;
+            b = 7788374;
+            uitkomst = ( a + b );
+        }
+
+        @Override
+        public void paint(Graphics g) {
+            g.drawString("2147483647 + 7788374 = " + uitkomst, 20, 20);
+        }
+    }
+
+
