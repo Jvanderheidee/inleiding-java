91823013a6246a4f1e5189a1f0e2af9e419da58c
justin <jvanderheide@roc-dev.com>
2019-09-26 12:49:36 +0200

    Ho2

---
+++ src/H11/vierkant.java
@@ -0,0 +1,32 @@
+package H11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+
+public class vierkant extends Applet {
+
+
+    @Override
+    public void init() {
+        setSize(1920,1080);
+
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        int y = 50;
+        int x = 50;
+        int width = 50;
+        int height = 50;
+
+        for (int i = 1; i <6; i++) {
+           g.drawRect(x,y,width,height);
+           x += width;
+           y += height;
+            
+        }
+
+    }
+}
