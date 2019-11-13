7120fbdad0e52b6a5c425cedb380e4cc95144052
justin <jvanderheide@roc-dev.com>
2019-11-13 16:35:41 +0100

    Ho2

---
+++ src/h14/kaartverdelen.java
@@ -0,0 +1,21 @@
+package h14;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class kaartverdelen extends Applet {
+  TextField [] tekstvakken;
+  Button deelkaarten;
+
+    public void init() {
+        tekstvakken = new TextField[1];
+
+    }
+
+
+    public void paint(Graphics g) {
+
+    }
+}
+
+
